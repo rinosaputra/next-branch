35aeffa1ccc9915683314e01e2f3f711d83f9756
Rino Saputra <rinosaputraas@gmail.com>
2026-02-16 10:33:30 +0700

    prisma: update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,9 +2,19 @@ import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../.generated/prisma/client'
 
-const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+});
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient;
+};
+
+const prisma =
+    globalForPrisma.prisma || new PrismaClient({
+        adapter,
+    });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export { prisma }
