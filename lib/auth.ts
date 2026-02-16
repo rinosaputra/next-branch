3efa75b861e9bc33dacd4d4879cc0549a2376331
Rino Saputra <rinosaputraas@gmail.com>
2026-02-16 10:42:51 +0700

    prisma: add better-auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,7 +3,10 @@ import { prismaAdapter } from 'better-auth/adapters/prisma'
 import { prisma } from '@/lib/prisma'
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: 'postgresql',
-    }),
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  emailAndPassword: {
+    enabled: true,
+  }
 })
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,17 +3,17 @@ import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../.generated/prisma/client'
 
 const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.DATABASE_URL!,
 });
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient;
+  prisma: PrismaClient;
 };
 
 const prisma =
-    globalForPrisma.prisma || new PrismaClient({
-        adapter,
-    });
+  globalForPrisma.prisma || new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
