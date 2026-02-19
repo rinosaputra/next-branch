66b505a7331adf2114510f03aab1b0d4b7164368
Rino Saputra <rinosaputraas@gmail.com>
2026-02-20 02:43:01 +0700

    auth-ui: add font-sans on layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
+    <div className="grid min-h-svh lg:grid-cols-2 font-sans">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <Link href="/" className="flex items-center gap-2 font-medium">
