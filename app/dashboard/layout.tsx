345a41a1733bdee6814036249c02792adaa7b643
Rino Saputra <rinosaputraas@gmail.com>
2026-02-22 02:24:02 +0700

    dashboard-foundation: add default layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -23,13 +23,7 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
         <div className="flex flex-1">
           <AppSidebar user={session.user} />
           <SidebarInset>
-            <div className="flex flex-1 flex-col">
-              <div className="@container/main flex flex-1 flex-col gap-2">
-                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                  {children}
-                </div>
-              </div>
-            </div>
+            {children}
           </SidebarInset>
         </div>
       </SidebarProvider>
